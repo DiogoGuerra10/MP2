464bf0a68382ac899068d47683a9fda3f79758f3
DiogoGuerra10 <d.guerra@ua.pt>
2024-11-24 14:54:19 +0000

    Update ESLint configuration to include React version detection and improve global settings

--- mini2-app/eslint.config.mjs
+++ mini2-app/eslint.config.mjs
@@ -2,7 +2,16 @@ import globals from 'globals';
 import pluginReact from 'eslint-plugin-react';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,jsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  { 
+    files: ['**/*.{js,mjs,cjs,jsx}'], 
+    languageOptions: { 
+      globals: globals.browser 
+    }, 
+    settings: { 
+      react: {
+        version: 'detect' 
+      }
+    }
+  },
+  pluginReact.configs.flat.recommended, 
 ];
